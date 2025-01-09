313fd5497273026f28ba23eb182342bb3542e86e
Hanna Kavaliova <Hanna.kavaliova83@gmail.com>
2025-01-09 12:47:41 +0100

    user component

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,25 @@
+import { Component, inject } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import {DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [ MatIconModule, MatButtonModule, MatTooltipModule, MatDialogModule],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.scss'
+})
+export class UserComponent {
+
+  readonly dialog = inject(MatDialog);
+
+  openDialog(){
+    this.dialog.open(DialogAddUserComponent);
+  }
+
+}
+
+
