9929ef5bf3fb6287db9da00d0d1bc83d3d1c2430
Hanna Kavaliova <Hanna.kavaliova83@gmail.com>
2025-01-21 10:50:09 +0100

    added Firebase configuration and integrated Firestore; updated routing to include user detail view

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,22 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDk5-eYvlwqyOm1CM-z50a0dX0Kr1XBT9w",
+  authDomain: "simple-crm-8428e.firebaseapp.com",
+  projectId: "simple-crm-8428e",
+  storageBucket: "simple-crm-8428e.firebasestorage.app",
+  messagingSenderId: "718576740179",
+  appId: "1:718576740179:web:901283cd06970be2ace3a6"
+};
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp(firebaseConfig)), provideFirestore(() => getFirestore())]
 };
+
+
+
+
