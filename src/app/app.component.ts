a7224c1f3f68a278fc7b2561545648d7fd5ec921
Hanna Kavaliova <Hanna.kavaliova83@gmail.com>
2025-01-21 10:49:42 +0100

    added MatProgressBar and MatCard modules to app component imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,15 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatCardModule} from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatToolbarModule, MatSidenavModule, MatIconModule, MatButtonModule, MatTooltipModule],
+  imports: [RouterOutlet, MatToolbarModule, MatSidenavModule, MatIconModule, MatCardModule,
+  MatProgressBarModule, MatButtonModule, MatTooltipModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
