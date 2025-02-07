2c014165dab7d00869b43892e56ae8fa5c2b93a2
Hanna Kavaliova <Hanna.kavaliova83@gmail.com>
2025-02-07 14:05:00 +0100

    add user type and priority columns to user table; enhance styling and layout

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { Firestore, collection, addDoc, updateDoc, doc } from '@angular/fire/firestore';
+import { Firestore, collection, addDoc, getDocs, updateDoc, doc } from '@angular/fire/firestore';
 import { RouterLink } from '@angular/router';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
@@ -10,12 +10,14 @@ import {DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.compon
 import { User } from '../../models/user.class';
 import { collectionData } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
-
+import { UserService } from '../user.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [ MatIconModule, MatButtonModule, MatTooltipModule, MatDialogModule, MatCardModule, RouterLink],
+  imports: [ MatIconModule, MatButtonModule, MatTooltipModule, 
+    MatDialogModule, MatCardModule, RouterLink, NgClass],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
@@ -28,7 +30,7 @@ export class UserComponent {
 
 
 
-  constructor() {
+  constructor(private userService: UserService) {
     this.users$ = collectionData(collection(this.firestore, 'users'), { idField: 'id' }) as Observable<User[]>;
   }
 
@@ -39,39 +41,48 @@ export class UserComponent {
     });
   }
 
+
   // async loadUsers() {
   //   const querySnapshot = await getDocs(collection(this.firestore, 'users'));
   //   this.users = querySnapshot.docs.map(doc => doc.data() as User);
   //   console.log('Loaded users:', this.users); // Debug log
   // }
 
-  async saveUsers(user: User) {
-    try {
-      const usersCollection = collection(this.firestore, 'users');
-      const docRef = await addDoc(usersCollection, user.toJSON());
-      console.log('User saved with ID:', docRef.id);
+  // async saveUsers(user: User) {
+  //   try {
+  //     const usersCollection = collection(this.firestore, 'users');
+  //     const docRef = await addDoc(usersCollection, user.toJSON());
+  //     console.log('User saved with ID:', docRef.id);
 
-      // Update the user with the generated ID
-      const userDoc = doc(this.firestore, `users/${docRef.id}`);
-      await updateDoc(userDoc, { id: docRef.id });
+  //     // Update the user with the generated ID
+  //     const userDoc = doc(this.firestore, `users/${docRef.id}`);
+  //     await updateDoc(userDoc, { id: docRef.id });
 
-    } catch (e) {
-      console.error('Error adding document: ', e);
-    }
-  }
+  //   } catch (e) {
+  //     console.error('Error adding document: ', e);
+  //   }
+  // }
 
   readonly dialog = inject(MatDialog);
 
   openDialog(){
     const dialogRef = this.dialog.open(DialogAddUserComponent);
    
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('Dialog result:', result); // Debug log
+    // dialogRef.afterClosed().subscribe(result => {
+    //   console.log('Dialog result:', result); // Debug log
 
-      if (result) {
-        this.saveUsers(result);
-      }
-    });
+    //   if (result) {
+    //     this.saveUsers(result);
+    //   }
+    // });
+  }
+
+  getTypeClass(type: string): string {
+    return this.userService.getTypeClass(type);
+  }
+
+  getPriorityClass(priority: string): string {
+    return this.userService.getPriorityClass(priority);
   }
 
 }
