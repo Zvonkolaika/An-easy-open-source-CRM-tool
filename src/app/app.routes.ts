31c5439ac760f123944b4431491897d0d113d2b8
Hanna Kavaliova <Hanna.kavaliova83@gmail.com>
2025-02-11 15:05:32 +0100

    add route for DealDetail component to handle individual deal views

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,14 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserComponent } from './user/user.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
 import { DealComponent } from './deal/deal.component';
+import { DealDetailComponent } from './deal-detail/deal-detail.component';
 
 export const routes: Routes = [
     { path: '', component: DashboardComponent},
     { path: 'dashboard', component: DashboardComponent},
     { path: 'user', component: UserComponent},
     { path: 'user/:id', component: UserDetailComponent},
-    { path: 'deal', component: DealComponent}
+    { path: 'deal', component: DealComponent},
+    { path: 'deal/:id', component: DealDetailComponent},
+    
 ];
