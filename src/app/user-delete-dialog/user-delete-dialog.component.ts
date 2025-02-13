fc693ec62d06e88908609025d8e9f1d4611acb37
Hanna Kavaliova <Hanna.kavaliova83@gmail.com>
2025-02-13 15:07:12 +0100

    add user delete confirmation dialog component with template, styles, and tests

---
+++ src/app/user-delete-dialog/user-delete-dialog.component.ts
@@ -0,0 +1,28 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import {MatButtonModule} from '@angular/material/button';
+import {
+  MatDialogActions,
+  MatDialogContent,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-user-delete-dialog',
+  standalone: true,
+  imports: [MatButtonModule, MatDialogActions, MatDialogContent, MatDialogTitle],
+  templateUrl: './user-delete-dialog.component.html',
+  styleUrl: './user-delete-dialog.component.scss'
+})
+export class UserDeleteDialogComponent {
+
+  constructor(
+    public dialogRef: MatDialogRef<UserDeleteDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { message: string }
+  ) {}
+
+  onClose(): void {
+    this.dialogRef.close();
+  }
+
+}
